96763881203221642bf9df670803553b9c9a4f27
neumb <neumb@localhost>
2024-04-03 16:30:01 +0300

    2023 day 19 part 1 in go

---
+++ aoc_2023_19_go/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type RuleClause struct {
+	Lhs string
+	Op  string
+	Rhs int
+}
+
+type WorkflowRule struct {
+	Clause *RuleClause
+	Then   string
+}
+
+func WorkflowRuleFromString(s string) WorkflowRule {
+	if !strings.Contains(s, ":") {
+		return WorkflowRule{nil, s}
+	}
+
+	// contains clause
+	if strings.Contains(s, "<") {
+		lhs, rhs, _ := strings.Cut(s, "<")
+		rhs, then, _ := strings.Cut(rhs, ":")
+		rhsInt, err := strconv.Atoi(rhs)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		clause := &RuleClause{lhs, "<", rhsInt}
+		return WorkflowRule{clause, then}
+	} else {
+		lhs, rhs, _ := strings.Cut(s, ">")
+		rhs, then, _ := strings.Cut(rhs, ":")
+		rhsInt, err := strconv.Atoi(rhs)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		clause := &RuleClause{lhs, ">", rhsInt}
+		return WorkflowRule{clause, then}
+	}
+}
+
+type Workflow struct {
+	Name  string
+	Rules []WorkflowRule
+}
+
+func WorkflowFromString(s string) Workflow {
+	split := strings.SplitAfter(s, "{")
+	workflowName, rhs := strings.TrimRight(split[0], "{"), strings.TrimRight(split[1], "}")
+
+	ruleStrings := strings.Split(rhs, ",")
+
+	rules := make([]WorkflowRule, 0, 2)
+
+	for _, rs := range ruleStrings {
+		rules = append(rules, WorkflowRuleFromString(rs))
+	}
+
+	return Workflow{workflowName, rules}
+}
+
+type Part map[string]int
+
+func PartFromString(s string) Part {
+	p := make(Part)
+	props := strings.Split(strings.Trim(s, "{}"), ",")
+	for _, prop := range props {
+		name, val, _ := strings.Cut(prop, "=")
+		valInt, err := strconv.Atoi(val)
+		if err != nil {
+			log.Fatal(err)
+		}
+		p[name] = valInt
+	}
+	return p
+}
+
+func (p Part) Sum() int {
+	var sum int
+	for _, v := range p {
+		sum += v
+	}
+	return sum
+}
+
+type AoC struct {
+	Workflows map[string]Workflow
+	Parts     []Part
+}
+
+func parse(input string) AoC {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	wrkflws := make(map[string]Workflow)
+
+	var cur int
+
+	for cur < len(lines) {
+		l := lines[cur]
+		if len(l) == 0 {
+			cur++
+			break
+		}
+		wrkflw := WorkflowFromString(l)
+		wrkflws[wrkflw.Name] = wrkflw
+		cur++
+	}
+	parts := make([]Part, 0, len(lines)-len(wrkflws)-1)
+
+	for cur < len(lines) {
+		l := lines[cur]
+		parts = append(parts, PartFromString(l))
+		cur++
+	}
+
+	return AoC{
+		wrkflws,
+		parts,
+	}
+}
+
+func (a *AoC) CheckPart(p Part) bool {
+	//fmt.Println("checking part", p)
+	wrkflw := a.Workflows["in"]
+
+outer:
+	for {
+		//fmt.Println(wrkflw.Name)
+		for _, r := range wrkflw.Rules {
+			if r.Clause != nil {
+				lhsVal := p[r.Clause.Lhs]
+				var clauseSatisfied bool
+				switch r.Clause.Op {
+				case ">":
+					clauseSatisfied = lhsVal > r.Clause.Rhs
+				case "<":
+					clauseSatisfied = lhsVal < r.Clause.Rhs
+				default:
+					panic("invalid clause operator")
+				}
+
+				if clauseSatisfied {
+					if len(r.Then) == 1 {
+						return r.Then == "A"
+					} else {
+						wrkflw = a.Workflows[r.Then]
+						continue outer
+					}
+				}
+
+			} else if len(r.Then) == 1 {
+				return r.Then == "A"
+			} else {
+				wrkflw = a.Workflows[r.Then]
+				continue outer
+			}
+		}
+	}
+}
+
+func (a *AoC) part1() any {
+	var res int
+	for _, p := range a.Parts {
+		if a.CheckPart(p) {
+			res += p.Sum()
+		}
+	}
+	return res
+}
+
+func (a *AoC) part2() any {
+	return 0
+}
+
+func main() {
+	//content, err := os.ReadFile("test-input.txt")
+	content, err := os.ReadFile("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	aoc := parse(string(content))
+
+	fmt.Println("part1:", aoc.part1())
+	fmt.Println("part2:", aoc.part2())
+}
